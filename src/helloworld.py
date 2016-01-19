d927eae4f8ed9310dc57d483ac08c747d0e4f882
Mac <Mac@s172-20-17-h205.paws.uga.edu>
2016-01-19 16:19:23 -0500

    Testing netbeans commit

---
+++ src/helloworld.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Mac"
+__date__ = "$Jan 19, 2016 4:16:05 PM$"
+
+if __name__ == "__main__":
+    print "Hello World ok iy"
